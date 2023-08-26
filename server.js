3a983cbf1e87716a7985824b7e09db51e5e42354
Fagner Joaquim <131827373+Fagner2002@users.noreply.github.com>
2023-08-26 19:40:53 -0300

    Nodejs
    - Aprendendo a fazer requisições

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mysql = require('mysql');
+const path = require('path');
+
+const app = express();
+
+const con = mysql.createConnection({
+  host: "localhost",
+  port: 3307,
+  user: "root",
+  password: "123456",
+  database: "mydb"
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.post('/insertTeste', (req, res) => {
+  console.log('entrou no servidor');
+  // Aqui é onde você deve realizar a lógica para inserir os dados e enviar uma resposta ao cliente.
+  res.json({ message: 'Requisição simples' });
+});
+
+app.get('/insertData', (req, res) => {
+  const query = "INSERT INTO customers (name, address) VALUES ('teste', 'teste')";
+
+  con.query(query, (err, result) => {
+    if (err) {
+      console.error("Erro ao inserir dados:", err);
+      res.status(500).json({ message: "Erro ao inserir dados" });
+    } else {
+      console.log("Dados inseridos com sucesso!");
+      res.json({ message: "Dados inseridos com sucesso!" });
+    }
+  });
+});
+
+const PORT = 8081;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
